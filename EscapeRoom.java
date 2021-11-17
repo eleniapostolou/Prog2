348a7d20b72f7cbcd1daf68c0c8154bd82235412
elenintousi <93617657+elenintousi@users.noreply.github.com>
2021-11-17 22:40:20 +0200

    Create EscapeRoom.java
    Main class

---
+++ EscapeRoom.java
@@ -0,0 +1,19 @@
+package mysteryLab;
+
+public class EscapeRoom {
+
+public static void main (String[] args) {
+  
+ String[][] questions = new questions[4][18];
+ String[][] answers = new answers[4][18];
+  
+  questions[0][0] = "Πλάσμα που χαρακτηρίζεται από θεϊκή ομορφιά και υπέροχη φωνή";
+  
+  
+ 
+
+
+
+}
+
+}
