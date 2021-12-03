5a6de5eef437d2e9d89385c9d88f1d158787b985
Vasilikii <94742779+Vasilikii@users.noreply.github.com>
2021-12-03 20:35:45 +0200

    Update GuessWho.java

--- GuessWho.java
+++ GuessWho.java
@@ -10,6 +10,7 @@ public class GuessWho {
     private boolean outofGuesses = false;
 
     public GuessWho(String[] quest, String answer) {
+    //Αρχικοποιούμε την name με το όνομα του ατόμου που αποκρύπτεται και τον πίνακα με τα αντίστοιχα hints 
         name = answer;
         Quenstions[0] = quest[0];
         Quenstions[1] = quest[1];
@@ -33,7 +34,9 @@ public class GuessWho {
 
     public void startGuessing() {
         String guess;
-        while(!guess.equals(name)&& !outofGuesses  && /*timer>0*/) {
+	//Όσο το όνομα που βρίσκει ο παίκτης διαφέρει από το ζητούμενο και έχει προσπαθήσει ήδη λιγότερες από τρεις φορές και ο χρόνος δεν έχει λήξει συνεχίζει να παίζει 
+        while(!guess.equals(name) && !outofGuesses  && /*timer>0*/) {
+        //Του δίνουμε την επιλογή να πάρει extra hint ή να μαντέψει
             Object[] options = {"Extra Hint",
                     "Μάντεψε"};
             int n = JOptionPane.showOptionDialog(frame,
@@ -44,35 +47,41 @@ public class GuessWho {
                     null,
                     options,
                     options[1]);
+	//Αν θέλει extra hint του εμφανίζουμε το αμέσως επόμενο που βρίσκεται στον πίνακα και αυξάνουμε τον μετρητή	
             if (n == JOptionPane.YES_OPTION) {
-                JOptionPane.showMessageDialog(null, Questions[QuestionCount]);
+	        if(QuestionCount < guessLimit) {
+                     JOptionPane.showMessageDialog(null, Questions[QuestionCount]);
+		     QuestionCount++;
+		} else {
+                    outofGuesses = true;
+		    String message = String.format("Δεν έχεις άλλα hints, μάντεψε ποιός είναι.");
+                    JOptionPane.showMessageDialog(null, message);	
+                }	
+        //Αν θέλει να μαντέψει ελέγχουμε αν έχει λιγότερες απο τρεις ζωές και του ζητάμε να πληκτρολογήσει το όνομα		    
             } else if (n == JOptionPane.NO_OPTION) {
                 if(lives<3) {
-                if(QuestionCount < guessLimit) {
                     String who = JOptionPane.showInputDialog(null, "Γράψε ένα όνομα");
+	//Αν μάντεψε σωστά το όνομα, εμφανίζουμε αντίατοιχο μήνυμα και βγαίνει από την while 		
                     if(who == name) {
                         String message = String.format("Μπράβο σου κέρδισες!");
                         JOptionPane.showMessageDialog(null, message);
+	//Αν μάντεψε λάθος όνομα, εμφανίζουμε αντίστοιχο μήνυμα και αυξάνουμε τις ζωές του		    
                     } else {
                         String message = String.format("Λάθος Απάντηση.");
                         JOptionPane.showMessageDialog(null, message);
-                        QuestionCount++;
                         lives++;
                     }
-                } else {
-                    outofGuesses = true;
-                }
-                if(outofGuesses) {
+	//Αν συμπλήρωσε 3 προσπάθειες λάθος του βγάζουμε μήνυμα ότι έχασε		
+		} else {
                     String message = String.format("Ωχ.. έχασες! Δεν έχεις άλλες ζωές.");
                     JOptionPane.showMessageDialog(null, message);
+		    break;	
                 }
                 /*else {
                     String message = String.format("Μπράβο σου κέρδισες!");
                     JOptionPane.showMessageDialog(null, message);
-                }*/
-		        } else {
-                    break;
-                }
+	        }  */
+	//Αν πατήσει οπουδήποτε αλλού εκτός από τις δύο επιλογές που του δίνουμε του εμφανίζουμε κατάλληλο μήνυμα 	    
             } else {
                 setLabel("Παρακαλώ επιλέξτε.");
             }
