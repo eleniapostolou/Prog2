f943d76f6d2c34639504a63cb574fa84923496d6
elenintousi <93617657+elenintousi@users.noreply.github.com>
2021-12-11 18:21:59 +0200

    Update Countdown.java

--- Countdown.java
+++ Countdown.java
@@ -1,3 +1,16 @@
+*Countdown.java
+ *Copyright 2021 mysteryLab
+ */
+
+/**
+*The class Countdown creates and displays a countdown timer that calculates the time left 
+*for the player to complete the game. The timer is set to run for 45 minutes. When the 45 minutes pass,
+*the game is terminated and a message informing the user that the time is over shows up on the screen.
+*
+*
+*@version  _____
+*@author ELENI APOSTOLOU, ELENI NTOUSI 
+*/
 package mysteryLab;
 import javax.swing.*;
 import java.awt.*;
@@ -6,45 +19,50 @@ import java.util.TimerTask;
 
 public class Countdown {
 	
+	//A timer object
 	protected Timer timer;
+	//The time in seconds that the timer runs for
 	private int i;
 	
+	//Constructor
 	public Countdown() {
 		timer = new Timer();
 		i = 2700;
 		
 	}
 	
+	//Method geti(): Returns the value of i (the remaining time in seconds)
 	public int geti() {
 		
 		return i;
 	}
 	
-	    public void startCountdown() {
+	//Method startCountdown(): Creates and displays the countdown timer on the screen
+	public void startCountdown() {
 	    
-	        JFrame jframe = new JFrame();
-	        JLabel jLabel = new JLabel();
-	        jframe.setLayout(new FlowLayout());
-	        jframe.setBounds(500, 300, 400, 100);
+	    JFrame jframe = new JFrame();
+	    JLabel jLabel = new JLabel();
+	    jframe.setLayout(new FlowLayout());
+	    jframe.setBounds(500, 300, 400, 100);
 
-	        jframe.add(jLabel);
-	        jframe.setVisible(true);
+	    jframe.add(jLabel);
+	    jframe.setVisible(true);
 
-	        
+        timer.scheduleAtFixedRate(new TimerTask() {
 
-	        timer.scheduleAtFixedRate(new TimerTask() {
+    	 public void run() {
 
-	            public void run() {
+	            jLabel.setText("Time left: " + i/60 + ":" + i%60 + (i%60/10 == 0 ? "0" : "")); //Display the time in the form XX:XX
+	            i--;
 
-	                jLabel.setText("Time left: " + i/60 + ":" + i%60 + (i%60/10 == 0 ? "0" : ""));
-	                i--;
-
-	                if (i < 0) {
-	                    timer.cancel();
-	                    jLabel.setText("Time Over");
+	            if (i < 0) { //If the time is over cancel the timer and display a message saying "Time Over"
+	                timer.cancel();
+	                jLabel.setText("Time Over");
 	                  
-	                }
 	            }
-	        }, 0, 1000);
-	    }
+	        }
+	    }, 0, 1000);
 	}
+}
+
+
