aa8a9c5d3c39df50ce011721fe0c78fbabe3bff2
efkontoes <94440668+efkontoes@users.noreply.github.com>
2022-01-03 16:07:47 +0200

    Create TransitionManager_Efthymis.java

---
+++ TransitionManager_Efthymis.java
@@ -0,0 +1,60 @@
+package elenhs;
+ 
+public class TransitionManager {
+	UI ui;
+	public TransitionManager(UI userInt) {
+		ui = userInt;
+	}
+	
+	public void showMainScreen() {
+		
+		ui.titleNamePanel.setVisible(true);
+		ui.startButtonPanel.setVisible(true);
+		
+		ui.choiceButtonPanel.setVisible(false);
+		ui.welcomePanel.setVisible(false);
+		ui.inputPanel.setVisible(false);
+		ui.MtPanel.setVisible(false);
+		
+	}
+	
+	public void showRoomChoices() {
+		ui.titleNamePanel.setVisible(false);
+		ui.startButtonPanel.setVisible(false);
+		ui.welcomePanel.setVisible(false);
+		
+		ui.choiceButtonPanel.setVisible(true);
+		ui.inputPanel.setVisible(false);
+		ui.MtPanel.setVisible(false);
+	}
+	
+	public void welcomePlayer() {
+		ui.titleNamePanel.setVisible(false);
+		ui.startButtonPanel.setVisible(false);
+		ui.choiceButtonPanel.setVisible(false);
+		
+		ui.welcomePanel.setVisible(true);
+		ui.inputPanel.setVisible(false);
+		ui.MtPanel.setVisible(false);
+	}
+	
+	public void inputP() {
+		ui.titleNamePanel.setVisible(false);
+		ui.startButtonPanel.setVisible(false);
+		ui.choiceButtonPanel.setVisible(false);
+		
+		ui.inputPanel.setVisible(true);
+		ui.welcomePanel.setVisible(true);
+		ui.MtPanel.setVisible(false);
+	}
+	
+	public void roomPrep() {
+		ui.titleNamePanel.setVisible(false);
+		ui.startButtonPanel.setVisible(false);
+		ui.choiceButtonPanel.setVisible(false);
+		
+		ui.inputPanel.setVisible(true);
+		ui.welcomePanel.setVisible(false);
+		ui.MtPanel.setVisible(true);
+	}
+}
