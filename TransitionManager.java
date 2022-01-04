ffcc4c68cf1776354b372f71855b03758abb6256
elenintousi <93617657+elenintousi@users.noreply.github.com>
2022-01-04 17:20:03 +0200

    Update TransitionManager.java

--- TransitionManager.java
+++ TransitionManager.java
@@ -1,3 +1,8 @@
+package mysteryLab;
+
+public class TransitionManager {
+	
+	UI ui;
 	
 	public TransitionManager(UI userInt) {
 		ui = userInt;
@@ -10,7 +15,8 @@
 		
 		ui.choiceButtonPanel.setVisible(false);
 		ui.welcomePanel.setVisible(false);
-		
+		ui.inputPanel.setVisible(false);
+		ui.MtPanel.setVisible(false);
 		
 	}
 	
@@ -20,7 +26,8 @@
 		ui.welcomePanel.setVisible(false);
 		
 		ui.choiceButtonPanel.setVisible(true);
-		
+		ui.inputPanel.setVisible(false);
+		ui.MtPanel.setVisible(false);
 	}
 	
 	public void welcomePlayer() {
@@ -29,7 +36,30 @@
 		ui.choiceButtonPanel.setVisible(false);
 		
 		ui.welcomePanel.setVisible(true);
+		ui.inputPanel.setVisible(false);
+		ui.MtPanel.setVisible(false);
+	}
+	
+	public void inputP() {
+		ui.titleNamePanel.setVisible(false);
+		ui.startButtonPanel.setVisible(false);
+		ui.choiceButtonPanel.setVisible(false);
+		ui.MtPanel.setVisible(false);
+		
+		ui.inputPanel.setVisible(true);
+		ui.welcomePanel.setVisible(true);
 		
 	}
-
+	
+	public void roomPrep() {
+		ui.titleNamePanel.setVisible(false);
+		ui.startButtonPanel.setVisible(false);
+		ui.choiceButtonPanel.setVisible(false);
+		ui.cb.setVisible(false);
+		ui.welcomePanel.setVisible(false);
+		
+		ui.inputPanel.setVisible(true);
+		ui.MtPanel.setVisible(true);
+		
+	}
 }
