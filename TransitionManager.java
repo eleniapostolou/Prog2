5ed42cde9111fe3e94d6e2a59d5a466569687045
efkontoes <94440668+efkontoes@users.noreply.github.com>
2022-01-05 21:40:51 +0200

    Update TransitionManager.java

--- TransitionManager.java
+++ TransitionManager.java
@@ -1,22 +1,23 @@
 package mysteryLab;
 
+ 
 public class TransitionManager {
-	
 	UI ui;
-	
 	public TransitionManager(UI userInt) {
 		ui = userInt;
 	}
 	
 	public void showMainScreen() {
+		
+		ui.titleNamePanel.setVisible(true);
+		ui.startButtonPanel.setVisible(true);
+		
 		ui.choiceButtonPanel.setVisible(false);
 		ui.welcomePanel.setVisible(false);
 		ui.inputPanel.setVisible(false);
 		ui.MtPanel.setVisible(false);
 		ui.timePanel.setVisible(false);
 		
-		ui.titleNamePanel.setVisible(true);
-		ui.startButtonPanel.setVisible(true);
 		
 	}
 	
@@ -24,46 +25,55 @@ public class TransitionManager {
 		ui.titleNamePanel.setVisible(false);
 		ui.startButtonPanel.setVisible(false);
 		ui.welcomePanel.setVisible(false);
-		ui.timePanel.setVisible(false);
-		ui.inputPanel.setVisible(false);
-		ui.MtPanel.setVisible(false);
 		
 		ui.choiceButtonPanel.setVisible(true);
+		ui.inputPanel.setVisible(false);
+		ui.MtPanel.setVisible(false);
 	}
 	
 	public void welcomePlayer() {
 		ui.titleNamePanel.setVisible(false);
 		ui.startButtonPanel.setVisible(false);
 		ui.choiceButtonPanel.setVisible(false);
-		ui.timePanel.setVisible(false);
-		ui.inputPanel.setVisible(false);
-		ui.MtPanel.setVisible(false);
 		
 		ui.welcomePanel.setVisible(true);
+		ui.inputPanel.setVisible(false);
+		ui.MtPanel.setVisible(false);
+		ui.gob.setVisible(false);
+	
 	}
 	
 	public void inputP() {
 		ui.titleNamePanel.setVisible(false);
 		ui.startButtonPanel.setVisible(false);
 		ui.choiceButtonPanel.setVisible(false);
-		ui.MtPanel.setVisible(false);
 		
 		ui.inputPanel.setVisible(true);
 		ui.welcomePanel.setVisible(true);
-		ui.timePanel.setVisible(true);
-		
+		ui.MtPanel.setVisible(false);
 	}
 	
 	public void roomPrep() {
 		ui.titleNamePanel.setVisible(false);
 		ui.startButtonPanel.setVisible(false);
 		ui.choiceButtonPanel.setVisible(false);
-		ui.cb.setVisible(false);
-		ui.welcomePanel.setVisible(false);
 		
 		ui.inputPanel.setVisible(true);
+		ui.welcomePanel.setVisible(false);
 		ui.MtPanel.setVisible(true);
-		ui.timePanel.setVisible(true);
+		ui.cb.setVisible(false);
+		ui.mainTextArea.setFont(ui.miniGameFont);
+	}
+	public void resultPanel() {
+		ui.titleNamePanel.setVisible(false);
+		ui.startButtonPanel.setVisible(false);
+		ui.choiceButtonPanel.setVisible(false);
 		
+		ui.inputPanel.setVisible(false);
+		ui.welcomePanel.setVisible(false);
+		ui.MtPanel.setVisible(true);
+		ui.cb.setVisible(false);
+		ui.gob.setVisible(true);
+		ui.mainTextArea.setFont(ui.titleFont);
 	}
 }
