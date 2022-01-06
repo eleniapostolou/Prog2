f68af3ff21a8b5e528012084b7f61afc1e442cb6
efkontoes <94440668+efkontoes@users.noreply.github.com>
2022-01-06 11:12:27 +0200

    Create Countdown-final.java

---
+++ Countdown-final.java
@@ -0,0 +1,65 @@
+/*Countdown.java
+ *Copyright 2021 mysteryLab
+ */
+
+/**
+*The class Countdown creates and displays a countdown timer that calculates the time left 
+*for the player to complete the game. The timer is set to run for 45 minutes. When the 45 minutes pass,
+*the game is terminated and a message informing the user that the time is over shows up on the screen.
+*
+*
+*@version  _____
+*@author ELENI APOSTOLOU, ELENI NTOUSI 
+*/
+package mysteryLab;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Countdown {
+	
+	//A timer object
+	protected Timer timer;
+	//The time in seconds that the timer runs for
+	private int i;
+	UI ui;
+	TransitionManager tm;
+	
+	//Constructor
+	public Countdown(UI userInterface, TransitionManager tm) {
+		timer = new Timer();
+		i = 2700;
+		ui = userInterface;
+		this.tm = tm;
+		
+	}
+	
+	//Method geti(): Returns the value of i (the remaining time in seconds)
+	public int geti() {
+		
+		return i;
+	}
+	
+	//Method startCountdown(): Creates and displays the countdown timer on the screen
+	public void startCountdown() {
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+
+    	 public void run() {
+
+	            ui.timeLabel.setText("Time left: " + i/60 + ":" + (i%60/10 == 0 ? "0" : "") + i%60); //Display the time in the form XX:XX
+	            i--;
+
+	            if (i < 0) { //If the time is over cancel the timer and display a message saying "Time Over"
+	                timer.cancel();
+	                ui.timeLabel.setText("Time's Over");
+	                tm.resultPanel();
+	    			ui.mainTextArea.setText("\n     GAME OVER \n\n");
+	    			//ui.mainTextArea.append("   Ο χρόνος σας, τελείωσε...");
+	                  
+	            }
+	        }
+	    }, 0, 1000);
+	}
+}
+
