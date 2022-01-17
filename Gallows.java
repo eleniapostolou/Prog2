29000b0eda8e36a2ca34c40baf4fdb46aaa1c6f5
elenintousi <93617657+elenintousi@users.noreply.github.com>
2022-01-17 23:47:11 +0200

    Update Gallows.java

--- Gallows.java
+++ Gallows.java
@@ -12,168 +12,140 @@
 *@author EFTHYMIOS KONTOES , KOSTANTINOS SPATHAS 
 */
 
-package mysteryLab;
+package mysteryLab ;
+import java.util.Scanner ;
 
-public class Gallows extends Game {
-	
-    // Class fields
-	private String word;
+public class Gallows extends Game{
+
+
+        // Class fields
+	private String word ;
 	private char [] letters;
-	private char [] temp;
-	private int lives, j;
+	private char [] temp ;
+	private int lives,j;
 	private int leftlet;
-	private char [] unique;
-	UI ui; 
-	TransitionManager tm;
-	Countdown cd;
-	 
-	// Constructor 
-	public Gallows(int room, UI ui, TransitionManager tm, Countdown cd) {
+	private char [] unique ;
+	
+	// Constructor
+	public Gallows(int room) {
 		
-		word = answers[room][5];
-		letters = new char[word.length()];
-		unique = new char[24];
-		temp = new char[word.length()];
-		temp[0] = word.charAt(0);
+		word= answers[room][5];
+		letters= new char[ word.length()];
+		unique= new char[ word.length()];
+		temp = new char[ word.length()];
+		temp[0]=word.charAt(0);
+		for( int i = 1 ; i < word.length() ; i ++ ) {
+			temp[i] = '_' ; // it creates a table with only the first the letter of the word
+			unique[i]=	'\u0000';
+		}
+		for( int i=0 ; i < word.length() ; i ++) {
+			letters[i] =word.charAt(i);//it creates a char table wich contains the letters of the word
+		}
 		lives = 5;
 		leftlet = word.length() - 1;
-		j = 0;
+		j=0;
 		
-		this.ui = ui;
-		this.tm = tm;
-		this.cd = cd; 
-		
-		for (int i = 1; i < word.length(); i++) {
-			temp[i] = '_'; //it creates a table with only the first the letter of the word
-			unique[i] =	'\u0000';
-			letters[i] = word.charAt(i); //it creates a char table which contains the letters of the word
-		}
 		
 	}
 
 	// Method startGame: the main implementation of the Gallows game
-	public void playGame(String s) {
+	public boolean startGame() {
+
+		Scanner input = new Scanner( System.in );
+		boolean y ;
+		System.out.println("Συγχαρητήρια! βρίσκεστε στο 3ο επίπεδο! ");
+		System.out.println("Είστε ένα επίπεδο πριν τη τελευταία δοκιμασία αυτού του δωματίου...");
+		System.out.println("Ο γρίφος που καλείστε να λύσετε τώρα είναι μια κρεμάλα !");
+		System.out.println("Το παιχνίδι παίζεται ως εξής");
+		System.out.println("Υπάρχει μια κρυμμένη λέξη που καλείστε να βρείτε ...");
+		System.out.println("Σας δίνεται το πρώτο γράμμα της λέξης και πρέπει να μαντέψετε ποια άλλα γράμματα υπάρχουν μέσα σε αυτή !");
+		System.out.println("Έχετε περιθώριο 5 λαθών! Αν τα ξεπεράσετε τότε χάνετε ..."); // Να ρωτησουμε παιδια
+		System.out.println("Αν τα καταφέρετε τότε θα περάσετε στο 4ο και δυσκολότερο επίπεδο !!");
+		System.out.println("Η λέξη έχει τη μορφή ");
+		show();
+		System.out.println();
+		System.out.println(" Η κρεμάλα ξεκινάει ! καλή τύχη !");
+
+		do{
+			System.out.println("Μαντέψτε ένα γράμμα !");
+			System.out.println("Έχετε "+lives+"ζωές");
 		
-		boolean y;
-		ui.mainTextArea.setFont(ui.biggerFont);
-		
-		if (lives != 0 && leftlet != 0) {
-			
-			s = s.toUpperCase();
-			char gram = s.charAt(0);
-			ui.mainTextArea.setText(show()+"\n");
+			String a ;
+			a=input.next();
+			a=a.toUpperCase();
+			// data validation check
+			while(a.length() !=1) { 
+
+				System.out.println("Μη έγκυρη προσπάθεια ! Προσπαθήστε χρησιμοποιώντας μόνο ένα γράμμα");
+				a=input.next();
+			}
+			char gram=a.charAt(0);
 			y= search(gram);
-			ui.mainTextArea.setText(show()+"\n");
 			
 			// It keeps his guesses in check and it updates the proper variables
-			if (y) {
-				
-				if (leftlet != 0) {
-					
-					ui.mainTextArea.append("Το γράμμα υπάρχει στη λέξη!");
-					ui.mainTextArea.append("Έχετε "+lives+" ζωές \n");
-				
-				}
-				
+			if(y==true){
+				show();
+				System.out.println();
+				System.out.println("Το γράμμα υπάρχει στη λέξη!");
 			} else {
-				
+
 				lives -= 1;
-				ui.mainTextArea.append("Λάθος απάντηση ... \n");
-				
-				if (lives != 0) {
-					
-					ui.mainTextArea.append("Προσπαθήστε ξανά ! ");
-					ui.mainTextArea.append("Έχετε "+lives+" ζωές \n");
-					
+				System.out.print("Λάθος απάντηση ... ");
+				if(lives !=0){
+					System.out.println("Προσπθήστε ξανά !");
 				}
-				
 			}
-			
-		}
+
+		}while( lives != 0 && leftlet!=0 );
 		
-        // It prints the result of his effort
-		if (lives == 0) {
-			
-			ui.timePanel.setVisible(false);
-			tm.resultPanel(); 
-			ui.mainTextArea.setText("\n     GAME OVER \n\n");
-			cd.timer.cancel();
-			
-		} else if (leftlet == 0) { 
-			
-			ui.mainTextArea.append(" \n Βρήκατε τη λέξη! Περνάτε στο επόμενο επίπεδο");
-			EscapeRoom.miniGame = 4; 
-			ui.cb.setVisible(true);
-			
+                // It prints the result of his effort
+		if(lives == 0){
+			System.out.println();
+			System.out.println("Δυστυχώς δεν τα καταφέρατε , η λέξη ήταν "+ word );
+			return false;
+		} else {
+
+			System.out.println("Βρήκατε τη λέξη! Περνάτε στο επόμενο επίπεδο");
+			return true;
 		}
 
 	} 
         
 	// Method show : It prints the right guesses of the user and what is left to guess
-	private String show() {
-		
-		String s = "";
-		
-		for (int i = 0; i < word.length(); i++) {
-			s += temp[i] + " ";	
+	public void show() {
+
+		for( int i = 0 ; i < word.length() ; i ++ ) {
+			System.out.print(temp[i]+" ");
 		}
-		
-		return s;
-		
+
 	}
 
 	// Method search : It checks if the guess of the user is right and if it's so , it returns true
-	private boolean search(char g) {
+	public boolean search(char g) {
 
-		boolean t1 = false;
-		boolean t2 = false; 
+		boolean t1 = false ;
+		boolean t2 =false ; 
 
-		for (int k = 0; k < unique.length; k++) {
-			
-			if (unique[k] == g) {
-				t2 = true;
-			}
-			
-		}
 		
-		if (t2 == false || unique.length < 1) {
-			
-			for (int i = 1; i < word.length(); i++) {
-				
-				if (g == letters[i]) { 
-					
-					temp[i] = g;
-					t1 = true;
-					leftlet -= 1;
-					
+		for(int k =0 ; k<unique.length ; k++){
+			if(unique[k]==g){
+				t2=true;
+				System.out.println("Έχετε πει ξανά αυτό το γράμμα... Προσέχετε γιατί χάνετε ζωές");}
+		}
+			if(t2==false || unique.length<1){
+				for( int i =1 ; i < word.length() ; i++ ){
+					if (g == letters[i]){
+						temp[i] = g ;
+						t1 = true ;
+						leftlet-=1;
+					}
 				}
-				
+				unique[j]=g;
+				j++;
 			}
-			
-			unique[j] = g;
-			j++;
-			
-		}
 	
-		return t1;
-		
+		return t1 ;
 	}
-	
-	// Method printInstructions : It shows the instructions of the game 
-	public void printInstructions() {
-		
-		ui.mainTextArea.setText("Συγχαρητήρια! βρίσκεστε στο 3ο επίπεδο! \n"+
-		"Είστε ένα επίπεδο πριν τη τελευταία δοκιμασία αυτού του δωματίου...\n"+
-		"Ο γρίφος που καλείστε να λύσετε τώρα είναι μια κρεμάλα !\n"+
-		"Το παιχνίδι παίζεται ως εξής \n"+
-		"Υπάρχει μια κρυμμένη λέξη που καλείστε να βρείτε ...\n"+
-		"Σας δίνεται το πρώτο γράμμα της λέξης και πρέπει να μαντέψετε ποια άλλα γράμματα υπάρχουν μέσα σε αυτή !\n"+
-		"Έχετε περιθώριο 5 λαθών! Αν τα ξεπεράσετε τότε χάνετε ...\n"+
-		"Αν τα καταφέρετε τότε θα περάσετε στο 4ο και δυσκολότερο επίπεδο !!\n"+
-		"Η κρεμάλα ξεκινάει ! καλή τύχη !\n"+
-		"Η λέξη έχει τη μορφή \n"+show());
-		
-	}
+
 
 }
-
