708d919567a61158e9784781125b5b290c829440
elenintousi <93617657+elenintousi@users.noreply.github.com>
2022-01-17 20:09:36 +0200

    Update GuessWho.java

--- GuessWho.java
+++ GuessWho.java
@@ -1,152 +1,103 @@
-package Gameplay;
-import javax.swing.JOptionPane;
-/*Crossword.java
+/*GuessWho.java
  *Copyright 2021 mysteryLab
  */
 
 /**
-*The class QuessWho extends superclass Game (main class). The class implements the guessing a name game.
+*The class GuessWho extends superclass Game (main class). The class implements the guessing a name game.
 *The Class displays 10 hints which help the player to find the secret person. The player has 3 tries to guess.
 *
 *
 *@version  _____
 *@author VASILIKI KARAMANOU , MARIOS LIAPIS FOTOU
 */
+package mysteryLab;
 public class GuessWho extends Game {
 	
     // class fields 
     private String name;
-    private String who;
-    private String answer;
-    private int column = 4;
+    private int column = 5;
     private int lives = 3;
-    private boolean win = false;
-    private String guess="null";
-    private int i = 1;
-    private boolean flag = true;
+    private boolean r;
+    TransitionManager tm;
+    Countdown cd;
+    UI ui;
     
 
     // Constructor
-    public GuessWho(int room) {
-    	/*Questions = new String[10];
-        for(int i =4; i<14 ; i++){
-        	Questions[i-4] = questions[room][i];*/
+    public GuessWho(int room,UI ui,TransitionManager tm,Countdown cd) {
         	name = answers[room][4];
+        	this.ui = ui;
+        	this.tm = tm;
+        	this.cd = cd;
         }
 
-    ///Method printInstructions : prints the game's instructions	
-    public void printInstructions() {
-        System.out.println("Η τελευταία δοκιμάσια απαιτεί να μαντέψετε το όνομα του μυθικού προσώπου");
-        System.out.println("Οι κανόνες του παιχνιδιού είναι η εξής:");
-        System.out.println("Σας δίνονται σταδιακά 10 hints. Σύμφωνα με αυτά πρέπει να βρείτε το πρόσωπο που αποκρύπτεται.");
-        System.out.println("Έχετε τρεις προσπάθειες.");
-        System.out.println("Καλή Επιτυχία!");
+    //Method printInstructions : prints the game's instructions	
+    public void printInstructions(int room) {
+        ui.mainTextArea.setText("Η δεύτερη δοκιμάσια απαιτεί να μαντέψετε το όνομα ενός μυθικού προσώπου \n"+
+        		"Οι κανόνες του παιχνιδιού είναι η εξής: \n"+
+        		"Σας δίνονται σταδιακά 10 hints. Σύμφωνα με αυτά πρέπει να βρείτε το πρόσωπο που αποκρύπτεται.\n"+
+        		"Έχετε τρεις προσπάθειες.\n"+
+        		"Καλή Επιτυχία! Ορίστε το πρώτο hint \n\n"+
+        		questions[room][column-1]);
     }
 
-	//Method chooseOption: lets the user pick a hint or guess the secret name
-	public int chooseOption() {
-		 Object[] options = {"Extra Hint",
-        "Μάντεψε"};
-		 int n = JOptionPane.showOptionDialog(null,
-        "Μαντέψτε ή πάρτε το επόμενο στοιχείο.",
-        "Try",
-        JOptionPane.YES_NO_OPTION,
-        JOptionPane.QUESTION_MESSAGE,
-        null,
-        options,
-        options[1]);
-		return n;
-	}
 	
-        //Method showHint: gives an extra hint to the user
-	public void showHint(int room, int column, int i) {
+      //Method showHint: gives an extra hint to the user
+      public void showHint(int room) {
 		
-		String msg = String.format("%dο Στοιχείο",i);
-		String hint = questions[room][column];
-		JOptionPane.showMessageDialog(null, msg);
-		JOptionPane.showMessageDialog(null, hint);
-		
+     	ui.mainTextArea.setFont(ui.miniGameFont);
+	if (column==5 || r){
+		ui.mainTextArea.setText("\n"+questions[room][column]);
+		r = false;
+	} else if (column <14) {
+		ui.mainTextArea.append("\n"+questions[room][column]);
+	} else {
+		ui.gwB.setVisible(false);
+		ui.mainTextArea.append("\n Δεν έχεις άλλα HINTS ,πρέπει να μαντέψεις ");
+	}
+	column+=1;
+	
 	} 
 	
-	//Method getAnswer: lets the user to type the secret name
-	public String getAnswer() {
-		
-		String who = JOptionPane.showInputDialog(null, "Γράψε ένα όνομα");
-		answer = who.toUpperCase();
-		return answer;
-	}
 	
-	//Method checkAnswer: checks if the answer that user gives is the correct one and returns boolean value true or false for eatch case
+	
+	//Method checkAnswer: checks if the answer that user gives is the correct one and returns boolean value true or false for each case
 	public boolean checkAnswer(String name, String who) {
 		
-		if(answer.equals(name) && lives>0) {
-			String message1 = String.format("Μπράβο σου κέρδισες!");
-			JOptionPane.showMessageDialog(null, message1);
+		if who.equals(name)) {
 			return true;
-			//Αν μάντεψε λάθος όνομα, εμφανίζουμε αντίστοιχο μήνυμα και αυξάνουμε τις ζωές του		    
-		
+			
 		} else {
-			String message1 = String.format("Λάθος Απάντηση.");
-			JOptionPane.showMessageDialog(null, message1);
+			lives--;
+			ui.mainTextArea.setText("\nΛάθος Απάντηση.\n");
+			ui.mainTextArea.append("Εχεις "+lives+" ζωες\n");	
+			ui.mainTextArea.setFont(ui.biggerFont);
+			r = true;
 			return false;
 		} 
 	}
     
-	//Method playGuessWho: the main implementation of the guessing game
-	public void playGuessWho(int room) {
+	//Method playGame: the main implementation of the guessing game
+	public void playGame(int room,String answer) {
 		
-		printInstructions();
-		
-		showHint(room, column, i);
-		
-		do {
-			
-			if(lives > 0 && i <10) {
+			boolean flag ;		
+			answer = answer.toUpperCase();
+			flag = checkAnswer(name, answer);
+			if (flag) {
+				ui.mainTextArea.setText("Μπράβο σου βρήκες το πρόσωπο και περνάς στο 3ο επίπεδο!");
+				EscapeRoom.miniGame = 3; 
+				ui.cb.setVisible(true);
+				ui.gwB.setVisible(false);
+				ui.mainTextArea.setFont(ui.biggerFont);
+			} else if (lives==0) {
+				ui.gwB.setVisible(false);
+				ui.timePanel.setVisible(false);
+				tm.resultPanel(); 
+				ui.mainTextArea.setText("\n     GAME OVER \n\n");
+				cd.timer.cancel();
 				
-				int option = ChooseOption();
-				
-					if (option == JOptionPane.YES_OPTION) {
-						
-						i++;
-						column++;
-						showHint(room, column, i);
-						
-				
-					} else {
-					
-						who = getAnswer();
-						win = checkAnswer(name, who);
-							if(win == false) {
-								lives--;
-								String message3 = String.format("Εχεις %d ζωες", lives);
-								JOptionPane.showMessageDialog(null, message3);
-							}
-					}
-		
-			} else if(lives == 0 && i < 10) {
-				String message3 = String.format("Δεν εχεις αλλες ζωες.Λυπαμαι εχασες!");
-				JOptionPane.showMessageDialog(null, message3);
-				break;
-				
-			} else if(lives > 0 && i == 10) {
-				if(flag == true) {
-					String message3 = String.format("Δεν έχεις αλλα hints");
-					JOptionPane.showMessageDialog(null, message3);
-					flag = false;
-				}
-				
-				who = getAnswer();
-				win = checkAnswer(name, who);
-					if(win == false) {
-						lives--;
-						String message3 = String.format("Εχεις %d ζωες", lives);
-						JOptionPane.showMessageDialog(null, message3);
-					}
-			}	
-
-		} while (win == false); 
-			
-		
+			}
+		  	
+	}				
 		
 }
-} 
