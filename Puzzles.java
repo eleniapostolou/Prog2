7457c19c507356727d43a70cbf67830fb7a6a396
elenintousi <93617657+elenintousi@users.noreply.github.com>
2022-01-17 20:11:44 +0200

    Update Puzzles.java

--- Puzzles.java
+++ Puzzles.java
@@ -11,96 +11,85 @@
 *@version ____
 *@author ELENI APOSTOLOU, KONSTANTINA SOTIROPOULOU
 */
-
-
 package mysteryLab;
-import java.util.Scanner;
 
 public class Puzzles extends Game {
-
+	
+	//Class fields
 	private int column = 15; //Refers to the position of the questions and answers at these tables.
 	private int counter = 0; //It will be used in order to define in which puzzle the player currently is.
-
-	public void instructions() {
-		System.out.println("Συγχαρητήρια! Έφτασες στο τελευταίο επίπεδο!");
-		System.out.println("Τώρα, για να αποδράσεις επιτέλους, πρέπει να λύσεις αυτούς τους τρεις γρίφους, χρησιμοποιώντας όσα έμαθες στα προηγούμενα στάδια.");
-		System.out.println("Χρησιμοποίησε τον χρόνο που σου απομένει έξυπνα! Ας ξεκινήσουμε και καλή επιτυχία! ");
+	TransitionManager tm;
+	Countdown cd;
+	UI ui;
+	
+	//Constructor
+	public Puzzles(UI ui, TransitionManager tm, Countdown cd) {
+		this.ui = ui;
+		this.tm = tm;
+		this.cd = cd;
 	}
 
-	public void getPuzzle(int room) { //We get the puzzle for each room.
-		System.out.println(questions[room][getColumn()]);
+	//Method printInstructions: The instructions are printed with the first puzzle.
+	public void printInstructions(int room) {
+		ui.mainTextArea.setText("Συγχαρητήρια! Έφτασες στο τελευταίο επίπεδο! \n"+
+		"Τώρα, για να αποδράσεις επιτέλους, πρέπει να λύσεις αυτούς τους τρεις γρίφους, χρησιμοποιώντας όσα έμαθες στα προηγούμενα στάδια. \n"+
+		"Χρησιμοποίησε τον χρόνο που σου απομένει έξυπνα! Ας ξεκινήσουμε και καλή επιτυχία! \n \n" + getPuzzle(room));
 	}
 
-	public void getResult(int room) {
+	//Method getPuzzle: It gets the puzzle of each room.
+	public String getPuzzle(int room) { 
+		 return questions[room][getColumn()];
+	}
 
-		//This is a method that interacts with the player. The player inputs his/her answer until he/she finds the correct one.
-		//We use a boolean flag in order to stop the loop
+	//Method getResult: It checks if the input is the correct one.
+	public void getResult(int room, String answer) {
+
 		//We use a counter in order to always keep track in which puzzle the player currently is.
+		String change = answer.toUpperCase();
 
-		Scanner in = new Scanner(System.in);
-		boolean flag = false;
-		String answer;
+		if (change.equals(answers[room][getColumn() - 9])) {		
+				
+			column = setColumn();
+			counter += 1;
 
-		while (flag == false) {
-
-			answer = in.nextLine();
-			String change = answer.toUpperCase();
-
-			if (change.equals(answers[room][getColumn() - 9])) {
-
-				flag = true;
-				column = setColumn();
-				counter = counter + 1;
-
-				if (counter < 3) {
-					System.out.println(" Συγχαρητήρια! Το έλυσες σωστα! Προχωράμε στον επόμενο γρίφο");
-				} else {
-					System.out.println(" Συγχαριτήρια! Έλυσες και τους τρεις γρίφους! Πέρασες όλες τις δοκιμασίες και απέδρασες από το δωμάτιο!" );
-				}
-
-
+			if (counter < 3) {
+				ui.mainTextArea.setText("Συγχαρητήρια! Το έλυσες σωστα! Προχωράμε στον επόμενο γρίφο");
 			} else {
-					System.out.println(" Ουπς... έκανες λάθος! Προσπάθησε πάλι!");
+				ui.timePanel.setVisible(false);
+				tm.resultPanel();
+				ui.mainTextArea.setText("\n\n YOU ARE A WINNER BABY !");
+				cd.timer.cancel();
 			}
 
+		} else {
+			ui.mainTextArea.setText(" Ουπς... έκανες λάθος! Προσπάθησε πάλι!");
 		}
+		
 	}
 
-	public int setColumn() { //We update our location at the tables.
+	//Method setColumn: It updates the location of the tables.
+	public int setColumn() { 
 
 		column = column + 1;
 		return column;
 	}
 
+	//Method getColumn: It returns the current column.
 	public int getColumn() {
 		return column;
 	}
 
-	public boolean playPuzzle(int room) { //This is the method that we are going to call in main in order to play the game.
-		//We call in repeat the puzzle and then the answer from the player until he/she finds all three or the time is up.
+	//Method playGame: the main implementation of the puzzle game.
+	public void playGame(int room, String answer) { 
 
-		boolean result = false;
-		instructions();
-
-
-		while (counter < 3) {
-
-			getPuzzle(room);
-			getResult(room);
-			result = true;
+		if (counter < 3) {
+			getResult(room, answer);
+			if (counter < 3) {
+			ui.mainTextArea.append("\n\n"+getPuzzle(room));
+			}
 		}
-
-		return result;
-
 	}
-
-
-
-
 }
-
-
-
 
 
 
